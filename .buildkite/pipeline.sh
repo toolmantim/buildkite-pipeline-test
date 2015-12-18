61ad91c583eb378425fe90ded8cf857f8414610b
Tim Lucas <t@toolmantim.com>
2015-12-18 18:00:07 +1100

    Update pipeline.sh

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,5 +5,8 @@ set -eu
 echo "steps:"
 
 for test_dir in test/*/; do
-  echo "  - command: \"run_test ${test_dir}\""
+  echo "  - command: \"run_test $test_dir\""
+  echo "    name: \":hammer: Test $test_dir\""
+  echo "    agents:"
+  echo "      ci: true"
 done
